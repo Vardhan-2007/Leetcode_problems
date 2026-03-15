c9982fa1c17d11fb9b3deccaa38b041ff64c701c
Vugiri Govardhan <vugirigovardhan@gmail.com>
2026-03-16 00:14:17 +0530

    Time: 56 ms (39.28%), Space: 19.2 MB (52.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,6 +2,8 @@ class Solution:
     def fib(self, n: int) -> int:
         if(n==0 or n==1):
             return n
+        if(n==2):
+            return 1
         f=0
         s=1
         while(n>=2):
