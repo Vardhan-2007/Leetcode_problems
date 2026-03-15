5ade31e5ac613b6da5c93aaa6ff12bf5bc382ff4
Vugiri Govardhan <vugirigovardhan@gmail.com>
2026-03-16 00:16:42 +0530

    Time: 39 ms (91.58%), Space: 19.1 MB (96.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -2,9 +2,11 @@ class Solution:
     def fib(self, n: int) -> int:
         if(n==0 or n==1):
             return n
-        f=0
+        if(n==2):
+            return 1
+        f=1
         s=1
-        while(n>=2):
+        while(n>=3):
             r=f+s
             f=s
             s=r
