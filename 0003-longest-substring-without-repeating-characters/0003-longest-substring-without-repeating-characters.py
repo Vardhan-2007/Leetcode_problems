cf5ff0ec755b7187bc4c58df581acefce973f926
Vugiri Govardhan <vugirigovardhan@gmail.com>
2026-07-02 23:08:57 +0530

    Time: 66 ms (11.66%), Space: 19.3 MB (29.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -10,7 +10,6 @@ class Solution:
                     longest = s1
                 i += 1
             else:
-                # Remove characters from the left
                 while s[i] in s1:
                     s1 = s1[1:]
 
