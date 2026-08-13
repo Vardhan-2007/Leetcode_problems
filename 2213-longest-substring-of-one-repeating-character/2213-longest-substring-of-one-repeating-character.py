d9ee12498c8899dab97809b97055818102be0d2b
Vugiri Govardhan <vugirigovardhan@gmail.com>
2026-08-13 21:37:11 +0530

    Time: 1890 ms (85.25%), Space: 43.4 MB (78.69%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def longestRepeating(
         self,
@@ -11,76 +10,102 @@ class Solution:
 
         n = len(s)
 
-        # Each node:
-        # [left_char, right_char, prefix, suffix, best, length]
-        tree = [None] * (4 * n)
+        # Segment tree size
+        size = 1
+        while size < n:
+            size <<= 1
 
-        def merge(a, b):
-            if a is None:
-                return b
-            if b is None:
-                return a
+        # For every node:
+        # left character
+        # right character
+        # prefix length
+        # suffix length
+        # best length
+        left = [''] * (2 * size)
+        right = [''] * (2 * size)
+        prefix = [0] * (2 * size)
+        suffix = [0] * (2 * size)
+        best = [0] * (2 * size)
+        length = [0] * (2 * size)
 
-            lc1, rc1, pre1, suf1, best1, len1 = a
-            lc2, rc2, pre2, suf2, best2, len2 = b
+        # Build leaves
+        for i, ch in enumerate(s):
+            p = size + i
 
-            same = rc1 == lc2
+            left[p] = right[p] = ch
+            prefix[p] = suffix[p] = best[p] = length[p] = 1
 
-            prefix = pre1
-            if pre1 == len1 and same:
-                prefix += pre2
+        # Build tree
+        for p in range(size - 1, 0, -1):
+            a = p << 1
+            b = a | 1
 
-            suffix = suf2
-            if suf2 == len2 and same:
-                suffix += suf1
+            left[p] = left[a]
+            right[p] = right[b]
+            length[p] = length[a] + length[b]
 
-            best = max(best1, best2)
+            same = right[a] == left[b]
+
+            prefix[p] = prefix[a]
+            if same and prefix[a] == length[a]:
+                prefix[p] += prefix[b]
+
+            suffix[p] = suffix[b]
+            if same and suffix[b] == length[b]:
+                suffix[p] += suffix[a]
+
+            best[p] = best[a]
+            if best[b] > best[p]:
+                best[p] = best[b]
 
             if same:
-                best = max(best, suf1 + pre2)
-
-            return (
-                lc1,
-                rc2,
-                prefix,
-                suffix,
-                best,
-                len1 + len2
-            )
-
-        def build(node, l, r):
-            if l == r:
-                ch = s[l]
-                tree[node] = (ch, ch, 1, 1, 1, 1)
-                return
-
-            mid = (l + r) // 2
-
-            build(node * 2, l, mid)
-            build(node * 2 + 1, mid + 1, r)
-
-            tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
-
-        def update(node, l, r, idx, ch):
-            if l == r:
-                tree[node] = (ch, ch, 1, 1, 1, 1)
-                return
-
-            mid = (l + r) // 2
-
-            if idx <= mid:
-                update(node * 2, l, mid, idx, ch)
-            else:
-                update(node * 2 + 1, mid + 1, r, idx, ch)
-
-            tree[node] = merge(tree[node * 2], tree[node * 2 + 1])
-
-        build(1, 0, n - 1)
+                x = suffix[a] + prefix[b]
+                if x > best[p]:
+                    best[p] = x
 
         ans = []
 
+        # Process queries
         for idx, ch in zip(queryIndices, queryCharacters):
-            update(1, 0, n - 1, idx, ch)
-            ans.append(tree[1][4])
+
+            p = size + idx
+
+            # Update leaf
+            left[p] = right[p] = ch
+            prefix[p] = suffix[p] = best[p] = length[p] = 1
+
+            # Update ancestors
+            p >>= 1
+
+            while p:
+                a = p << 1
+                b = a | 1
+
+                left[p] = left[a]
+                right[p] = right[b]
+                length[p] = length[a] + length[b]
+
+                same = right[a] == left[b]
+
+                prefix[p] = prefix[a]
+                if same and prefix[a] == length[a]:
+                    prefix[p] += prefix[b]
+
+                suffix[p] = suffix[b]
+                if same and suffix[b] == length[b]:
+                    suffix[p] += suffix[a]
+
+                best[p] = best[a]
+                if best[b] > best[p]:
+                    best[p] = best[b]
+
+                if same:
+                    x = suffix[a] + prefix[b]
+                    if x > best[p]:
+                        best[p] = x
+
+                p >>= 1
+
+            ans.append(best[1])
 
         return ans
